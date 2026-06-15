21416d914914a36ed6cf4f5ca36d07e778fc17af
Sam <thesammu09@gmail.com>
2026-06-15 13:07:24 -0400

    Initial commit

---
+++ src/Pacman.java
@@ -0,0 +1,18 @@
+
+import javax.swing.*;
+
+public class Pacman extends JFrame {
+
+    public Pacman() {
+        add(new Model());
+    }
+
+    public static void main(String[] args) {
+        Pacman pac = new Pacman();
+        pac.setTitle("Pacman");
+        pac.setSize(380, 420);
+        pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pac.setLocationRelativeTo(null);
+        pac.setVisible(true);
+    }
+}
